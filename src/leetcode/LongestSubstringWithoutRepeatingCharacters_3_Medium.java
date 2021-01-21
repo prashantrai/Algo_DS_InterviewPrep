91bc2b8e0e0645fc7a93e74a4497698f41c857b7
Prashant <prashantrai@Prashants-MBP.attlocal.net>
2021-01-20 23:08:05 -0800

    new code

--- src/leetcode/LongestSubstringWithoutRepeatingCharacters_3_Medium.java
+++ src/leetcode/LongestSubstringWithoutRepeatingCharacters_3_Medium.java
@@ -17,21 +17,32 @@ public class LongestSubstringWithoutRepeatingCharacters_3_Medium {
 		System.out.println("Expected: 3, Actual: " + lengthOfLongestSubstring(s));
 	}
 	
+	/*
+	 * Time: O(N)
+	 * Space: O(N) 
+	 */
+	
 	public static int lengthOfLongestSubstring(String s) {
         int i=0;
         int j=0;
         int max = 0;
+        String maxStr = "";
         Set<Character> set = new HashSet<>();
         
         while (j < s.length()) {
             if(!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j));
                 j++;
                 max = Math.max(max, set.size());
             } else {
+            	String currStr = s.substring(i,j);
+            	maxStr = maxStr.length() < currStr.length() ? currStr : maxStr;
                 set.remove(s.charAt(i));
                 i++;
             }
         }
+        
+        System.out.println("maxStr: " + maxStr);
         return max;
     }
 
