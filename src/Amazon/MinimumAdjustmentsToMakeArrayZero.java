34d462f9b7618b2f960fe67cd5afe2eb10b79ce3
Prashant <prashantrai@Prashants-MBP.lan>
2026-08-03 20:45:48 -0700

    new code

--- src/Amazon/MinimumAdjustmentsToMakeArrayZero.java
+++ src/Amazon/MinimumAdjustmentsToMakeArrayZero.java
@@ -3,7 +3,10 @@ import java.util.*;
 
 public class MinimumAdjustmentsToMakeArrayZero {
     
-    /**
+    /** Source: https://leetcode.com/discuss/post/6577750/amazon-interview-question-minimum-operat-nco3/
+     *  https://algo.monster/liteproblems/2772
+     *  https://leetcode.com/problems/apply-operations-to-make-all-array-elements-equal-to-zero/description/
+     *  
      * Minimum Adjustments to Make Array Zero
 
 		You are given an array and can perform operations where you select a prefix 
@@ -101,3 +104,36 @@ public class MinimumAdjustmentsToMakeArrayZero {
         return sum;
     }
 }
+
+
+/* Source: https://leetcode.com/discuss/post/6577750/amazon-interview-question-minimum-operat-nco3/
+ * 
+Problem Description
+You are given an integer array arr of size n. You need to perform operations on this array 
+to convert all its elements to 0.
+
+In one operation, you can select a prefix of the given array and increment or decrement all 
+the elements of the prefix by 1.
+
+A prefix is a contiguous subarray that includes the first element of the array. For example, in arr = [1, 2, 3, 4, 5], valid prefixes are: [1], [1, 2], [1, 2, 3], [1, 2, 3, 4], [1, 2, 3, 4, 5].
+
+Find the minimum number of operations required to convert every element of the array to 0.
+
+Constraints:
+1 <= n <= 10^5
+-10^9 <= arr[i] <= 10^9
+
+Input
+n = 5
+arr = [3, 2, 0, 0, -1]
+
+Output
+result = 5
+Explanation:
+Select prefix length 2 and decrement → [2,1,0,0,−1]
+Select prefix length 2 and decrement → [1,0,0,0,−1]
+Select prefix length 1 and decrement → [0,0,0,0,−1]
+Select prefix length 4 and decrement → [-1,-1,-1,-1,−1]
+Select prefix length 5 and increment → [0,0,0,0,0]
+ * */
+ */
